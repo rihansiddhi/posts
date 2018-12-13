65b35ee5711116934dd04c15b8170da88063186c
rihan.s <rihan.s@rently.com>
2018-12-13 11:33:17 +0530

    Starting project posts using react-router

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends Component {
+  render() { 
+    return ( 
+      <div> Hello!! </div>
+     );
+  }
+}
+class Goodbye extends Component {
+  render() { 
+    return ( 
+      <div> GoodBye!! </div>
+     );
+  }
+}
+ 
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
